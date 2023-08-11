ad5c4dff8481bdffad07dc3088fea6410359feff
choeyunseo <choeyunseo4060@naver.com>
2023-08-12 00:38:22 +0900

    ㅎㅎ

--- hackathon/accounts/urls.py
+++ hackathon/accounts/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
     path("signup/", views.signup_view, name="signup"),
-    path("afterlogin/", views.after_login_view, name="afterlogin")
+    path("afterlogin/", views.after_login_view, name="afterlogin"),
 ]
